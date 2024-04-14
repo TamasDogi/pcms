a143394b79aa40d02638c6330761e09b79cbccdc
Tamas Dogi <info@tamasdogi.eu>
2024-04-14 17:37:48 +0200

    files has been updated

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('pages/welcome');
-    });
-});
 
 Route::get('/contents', function () {
     return view('pages/contents');
